c01a2c47b4bf33f798c11e145c626348e51083e2
Marcusss313 <marcogarro313@gmail.com>
2022-03-23 17:04:30 +0100

    r

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+var name = 3
+const tot = 69
+for(let i=0; i++; i<= 420)
+function yu() {
+    var dio = document.getElementById("to").innerText;
+    console.log(dio);
+    var input= document.getElementById("inpututente").value;
+    document.getElementById("ïnpututente").innerText = `ciao , $(input)`;
+}
